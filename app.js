ce823532a93c89dcaea64bbc649c5930a83f07ec
Shahnwaz <shahnwaza058@gmail.com>
2023-07-22 14:04:33 +0530

    cors added

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
+const cors = require("cors");
 const app = express();
 
 if (process.env.MODE_ENV != "production")
   require("dotenv").config({ path: "./config/config.env" });
 
 //Using Middlewares
-app.use(express.json({
-    limit: '50mb'
-  }
-));
-app.use(express.urlencoded({
-  limit: "50mb", 
-  extended: true
- }
-));
-app.use(cookieParser())
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+    extended: true,
+  })
+);
 
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 //Importing routes
 const post = require("./routes/Post");
 const user = require("./routes/user");
